c3e79a681150b23aace4a50bc63a15507b9aaa27
Lucas Schulz <Lucas_schulz@sluz.ch>
2022-10-28 17:00:30 +0200

    fix 2

--- main.py
+++ main.py
@@ -83,6 +83,14 @@ def move(game_state: typing.Dict) -> typing.Dict:
     # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     my_body = game_state['you']['body']
 
+    if my_head["x"] and ["y"]-1 == my_body:
+        is_move_safe["down"] = False
+    if my_head["x"]+1 and ["y"] == my_body:
+        is_move_safe["right"] = False
+    if my_head["x"]-1 and ["y"] == my_body:
+        is_move_safe["left"] = False
+    if my_head["x"] and ["y"]+1 == my_body:
+        is_move_safe["up"] = False
     for x in my_body:
         print(my_body)
 
