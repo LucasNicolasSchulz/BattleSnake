86b1170eb5c4686c49a07fb02c62d9b7b0543b78
Lucas Schulz <Lucas_schulz@sluz.ch>
2022-10-28 18:43:53 +0200

    test 1

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ def move(game_state: typing.Dict) -> typing.Dict:
     # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
     food = game_state['board']['food']
     
-    print(food)
+    print("food", food)
 
 
 
