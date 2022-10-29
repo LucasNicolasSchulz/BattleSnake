d1c16e4d86f5e51954f5a08553ca9970f2afe402
Lucas Schulz <Lucas_schulz@sluz.ch>
2022-10-29 14:34:50 +0200

    fix 1000000000000000000

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def end(game_state: typing.Dict):
 def move(game_state: typing.Dict) -> typing.Dict:
 
     #is_move_safe setzt die möglichen bewegungen auf True
-    is_move_safe = {"up": True, "down": True, "left": True, "right": True}
+    is_move_safe = {"up": False, "down": False, "left": False, "right": False}
     print("hallo")
 
     #Die vier verschiedenen Spielfelder werden gemacht für die vier möglichen bewegungen
